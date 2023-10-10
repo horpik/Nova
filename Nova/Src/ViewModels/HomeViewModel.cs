e72e72ac9c587d099bc484b95da0d78d7edb18e6
Daniil <horpuru@gmail.com>
2023-10-10 10:15:55 +0300

    Fix gitignor

--- Nova/Src/ViewModels/HomeViewModel.cs
+++ Nova/Src/ViewModels/HomeViewModel.cs
@@ -76,9 +76,36 @@ namespace Nova.ViewModels
         private List<TagData> GenerateTagData()
         {
             List<TagData> tagDataList = new List<TagData>();
-            tagDataList.Add(new TagData("Описание поля 1", "Замени меня"));
-            tagDataList.Add(new TagData("Описание поля 2", "Поле 2"));
-            tagDataList.Add(new TagData("Описание поля 3", "Поле 3"));
+            tagDataList.Add(new TagData("Название компании", "<CompanyName>"));
+            tagDataList.Add(new TagData("Полное название продукта", "<FullProductName>"));
+            tagDataList.Add(new TagData("Название продукта", "<ProductName>"));
+            tagDataList.Add(new TagData("Тип продукта кратко ПО/ППО/СПО", "<ProductTypeReduction>"));
+            tagDataList.Add(new TagData("Тип продукта именительный падеж", "<ProductTypeNC>"));
+            tagDataList.Add(new TagData("Тип продукта  родительский падеж", "<ProductTypePaC>"));
+            tagDataList.Add(new TagData("Тип продукта предложный падеж", "<ProductTypePrC>"));
+            tagDataList.Add(new TagData("Тип продукта творительный падеж", "<ProductTypeCrC>"));
+            tagDataList.Add(new TagData("Децимальный номер", "<DecimalNumber>"));
+            tagDataList.Add(new TagData("Дополнительное сокращение", "<AdditionalReduction>"));
+            tagDataList.Add(new TagData("Дополнительное сокращение полное", "<AdditionalReductionExplanation>"));
+            tagDataList.Add(new TagData("Предприятие-изготовитель", "<ManufacturingCompany>"));
+            tagDataList.Add(new TagData("Предприятие-разработчик", "<DeveloperCompany>"));
+            tagDataList.Add(new TagData("Название предприятия разработчика", "<DeveloperCompanyName>"));
+            tagDataList.Add(new TagData("Название предприятия разработчика расшифровка", "<DeveloperCompanyNameDecoding>"));
+            tagDataList.Add(new TagData("Лицензия на деятельность по технической защите конфиденциальной информации", "<LicenseForTechnicalProtectionCI>"));
+            tagDataList.Add(new TagData("Лицензия на деятельность по разработке и (или) производству средств защиты конфиденциальной информации", "<LicenseDevelopmentProtectingCI>"));
+            tagDataList.Add(new TagData("Описание продукта", "<ProductDescription>"));
+            tagDataList.Add(new TagData("Идентификация и аутентификация субъектов доступа и объектов доступа", "<IAO>"));
+            tagDataList.Add(new TagData("Управление доступом субъектов доступа к объектам доступа", "<ACO>"));
+            tagDataList.Add(new TagData("Управление доступом субъектов доступа к объектам доступа", "<RSE>"));
+            tagDataList.Add(new TagData("Класс защищённости", "<SecurityClass>"));
+            tagDataList.Add(new TagData("Уровень защищённости", "<SecurityLevel>"));
+            tagDataList.Add(new TagData("На каком сервере размещается", "<ApplicationServer>"));
+            tagDataList.Add(new TagData("В какой субд хранится", "<DBMSDataStorage>"));
+            tagDataList.Add(new TagData("Тип организации полностью", "<TypeOrganizationFull>"));
+            tagDataList.Add(new TagData("Тип организации кратко", "<TypeOrganizationShort>"));
+            tagDataList.Add(new TagData("Адрес", "<Address>"));
+            tagDataList.Add(new TagData("Сайт", "<Website>"));
+            tagDataList.Add(new TagData("Год", "<Year>"));
             return tagDataList;
         }
     }
